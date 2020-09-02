3a09ba863a43a37248610dda9a82c21e904d115f
roshan161 <65772348+roshan161@users.noreply.github.com>
2020-09-02 19:23:10 +0545

    Removed password

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6yhg@+3%_yd75ew%r#k4b8@8n_rt=qpm&mq)^of-d=fghtf0$c'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
